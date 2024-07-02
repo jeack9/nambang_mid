5926a2f2ed20f6867c02094e3de6d174ea1a4628
b5643 <b5643@DESKTOP-6P8QH1V>
2024-07-03 00:25:51 +0900

    0703 회원정보 변경

--- CheckNambang/src/main/java/co/nambang/web/Woon13.java
+++ CheckNambang/src/main/java/co/nambang/web/Woon13.java
@@ -5,17 +5,24 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import co.nambang.addr.service.AddrService;
 import co.nambang.addr.service.AddrServiceImpl;
 import co.nambang.addr.vo.AddrVO;
 import co.nambang.common.Control;
+import co.nambang.member.vo.MemberVO;
 
 public class Woon13 implements Control {
 
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		//HttpSession session = req.getSession();
+		//MemberVO login = (MemberVO)session.getAttribute("login");
+		//String userId = login == null ? "" : login.getUserId();
+		
 		String userId = req.getParameter("userId");
 		String addr2 = req.getParameter("addr2");
 		String getter = req.getParameter("getter");
