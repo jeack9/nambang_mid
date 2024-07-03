b805cb471b724e09d7f06ba48e57386a10941084
jeack9 <jeack99@g.yju.ac.kr>
2024-07-03 21:47:05 +0900

    장바구니 담기

---
+++ CheckNambang/src/main/java/co/nambang/product/web/ProductList.java
@@ -0,0 +1,20 @@
+package co.nambang.product.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.nambang.common.Control;
+
+public class ProductList implements Control {
+
+	@Override
+	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String proCode = req.getParameter("proCode");
+
+		req.getRequestDispatcher("product/product.tiles").forward(req, resp);
+	}
+
+}
