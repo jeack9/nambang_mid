1b83f28b769a23fb74203082cd0ba8afc8e902d8
b5643 <b5643@DESKTOP-6P8QH1V>
2024-07-07 23:37:21 +0900

    0707 수정 완료

--- CheckNambang/src/main/webapp/kdwjs/orderHistory.js
+++ CheckNambang/src/main/webapp/kdwjs/orderHistory.js
@@ -25,7 +25,7 @@ function cloneRow(cart ={}){
 	console.log(cart);
 	
 	let historyclone = document.querySelector('tbody>tr:nth-of-type(1)').cloneNode(true);
-	historyclone.querySelector('.shoping__cart__item>img').setAttribute('src', `img/${cart.productImage}`)
+	historyclone.querySelector('.shoping__cart__item>img').setAttribute('src', `image/${cart.productImage}`)
 	historyclone.querySelector('.shoping__cart__item_name>h5').innerHTML = cart.productName;
 	historyclone.querySelector('.shoping__cart__price').innerHTML = cart.opPrice + "원";
 	historyclone.querySelector('.shoping__cart__quantity').innerHTML = cart.opVolume;
